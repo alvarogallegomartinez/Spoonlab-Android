88447adbd02ebdd28ffcffa5fb8d293c2260526b
Álvaro Gallego Martínez <alvarok047@gmail.com>
2021-01-05 01:28:13 +0100

    Añadida parte de la lógica del registro

--- Proyecto/app/src/main/java/com/example/spoonlab/RegisterActivity.java
+++ Proyecto/app/src/main/java/com/example/spoonlab/RegisterActivity.java
@@ -1,14 +1,41 @@
 package com.example.spoonlab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+
+        // Autenticación con firebase
+        // @TODO: Añadir el mail y la contraseña con la info del usuario y activar el registro con el botón
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword("Mail", "Password")
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(getApplicationContext(), "Se ha registrado el usuario satisfactoriamente", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Ha habido un error al registrar al usuario", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
