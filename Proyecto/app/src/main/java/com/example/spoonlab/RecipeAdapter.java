6129b746fd23028f6a77ddf068238c84a3960d0c
Álvaro Gallego Martínez <alvarok047@gmail.com>
2021-02-08 00:56:28 +0100

    Estructura del Recycler View

---
+++ Proyecto/app/src/main/java/com/example/spoonlab/RecipeAdapter.java
@@ -0,0 +1,89 @@
+package com.example.spoonlab;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// TODO: Terminar de hacer el recycler view
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    List<Recipe> recipeList;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    ViewGroup parent;
+
+    public RecipeAdapter(List<Recipe> recipeList) {
+        this.recipeList = recipeList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_layout, parent,false);
+
+        database = FirebaseDatabase.getInstance();
+        this.parent = parent;
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String title = recipeList.get(position).getTitle();
+        String description = recipeList.get(position).getDescription();
+
+        holder.title.setText(title);
+        holder.description.setText(description);
+
+        LinkedList<String> ingredients = (LinkedList<String>) recipeList.get(position).getIngredients();
+
+        // Creo que de este modo ya funcionaría el añadir de forma dinámica los ingredientes
+        // TODO: Hacer debug de esta parte
+        for(String ingredient : ingredients) {
+            View checkIngredient = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.ingredient_view, null, false);
+
+            CheckBox ingredientCheckBox = checkIngredient.findViewById(R.id.Ingredient);
+            ingredientCheckBox.setText(ingredient);
+
+            holder.ingredientsLayout.addView(checkIngredient);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title, description;
+        LinearLayout ingredientsLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+
+            ingredientsLayout = itemView.findViewById(R.id.Ingredients);
+        }
+    }
+
+}
