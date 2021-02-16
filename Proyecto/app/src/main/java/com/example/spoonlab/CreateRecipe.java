dc512527ef2607841fd7e9e271dc0f151934f013
Alvaro Gallego Martinez <alvarok047@gmail.com>
2021-02-16 15:06:36 +0100

    Reubicada la funcionalidad de Sign Out

--- Proyecto/app/src/main/java/com/example/spoonlab/CreateRecipe.java
+++ Proyecto/app/src/main/java/com/example/spoonlab/CreateRecipe.java
@@ -3,6 +3,8 @@ package com.example.spoonlab;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.spoonlab.exceptions.DifferentPasswordException;
+import com.example.spoonlab.exceptions.EmptyTextException;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -10,8 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateRecipe extends AppCompatActivity {
+
+    private EditText name, ingredients, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +26,36 @@ public class CreateRecipe extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        description = findViewById(R.id.editSteps);
+        ingredients = findViewById(R.id.editIngredients);
+        name = findViewById(R.id.editName);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CreateRecipe.this, MainActivity.class);
-                startActivity(intent);
+                try {
+                    validateData();
+
+                    // Volver a la página principal
+                    Intent intent = new Intent(CreateRecipe.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "Recipe created successfully!", Toast.LENGTH_SHORT).show();
+                    //TODO: AÑADIR la receta al USUARIO
+
+                    finish();
+                } catch (EmptyTextException e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    e.getStackTrace();
+                }
+                catch (Exception ignored) {}
             }
         });
+    }
+
+    // Comprobar que no nos dejamos ningún campo vacío
+    private void validateData() throws Exception {
+        if (description.getText().toString().equals("") || ingredients.getText().toString().equals("") || name.getText().toString().equals(""))
+            throw new EmptyTextException();
     }
 }
